092678e5ce3c8c7284681aea7916bb96f0e3acb3
Lawrence Hunt <lawrence.hunt@gmail.com>
2025-05-25 05:23:29 +0000

    chore: Update config typing

--- custom_components/powerpal/__init__.py
+++ custom_components/powerpal/__init__.py
@@ -9,10 +9,11 @@ from datetime import timedelta
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import Config, HomeAssistant
+from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers.typing import ConfigType
 
 from mindmelting.powerpal import Powerpal
 
@@ -29,7 +30,7 @@ SCAN_INTERVAL = timedelta(seconds=60)
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
-async def async_setup(hass: HomeAssistant, config: Config):
+async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Set up this integration using YAML is not supported."""
     return True
 
