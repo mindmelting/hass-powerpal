8bd534ccabaf07bf1b04e1d0ce62897ec26bc562
Lawrence <lawrence.hunt@gmail.com>
2025-06-01 21:18:29 +1000

    chore: Update deprecated config functions (#32)
    * chore: Update deprecated config functions
    
    * chore: Modify async setup entry
    
    * chore: Modify async setup entry
    
    * chore: Update config typing

--- custom_components/powerpal/__init__.py
+++ custom_components/powerpal/__init__.py
@@ -9,10 +9,11 @@ from datetime import timedelta
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import Config, HomeAssistant
+from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers.typing import ConfigType
 
 from mindmelting.powerpal import Powerpal
 
@@ -29,7 +30,7 @@ SCAN_INTERVAL = timedelta(seconds=60)
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
-async def async_setup(hass: HomeAssistant, config: Config):
+async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Set up this integration using YAML is not supported."""
     return True
 
@@ -57,11 +58,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
 
-    entry.add_update_listener(async_reload_entry)
+    await coordinator.async_config_entry_first_refresh()
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+
     return True
 
 
